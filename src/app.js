f00e25a8361afeba5eb5252cdd9588fa23c14c0b
Miguel <miguelmorci@gmail.com>
2021-03-15 01:04:37 +0100

    erased src tone

--- src/app.js
+++ src/app.js
@@ -1,13 +1,3 @@
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioCtx = new AudioContext();
-let wave = audioCtx
-
 window.onload = function () {
-    playSweep()
-}
-
-function playSweep() {
-    let osc = audioCtx.createOscillator();
-
-    console.log(osc);
+   
 }
