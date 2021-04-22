f277d7b351875fd856de85319000015a289c351d
Miguel <miguelmorci@gmail.com>
2021-04-22 17:55:55 +0200

    probado un c4

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
-window.onload = function () {
-   
-}
+$(document).ready(function init() {
+    console.log("HOLA?");
+    $(".button").click(function () {
+        ev.preventDefault()
+        const synth = new Tone.Synth().toDestination();
+        while (true) {
+            synth.triggerAttackRelease("C4", "8n");
+        }
+    })
+})
