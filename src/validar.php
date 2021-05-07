fc69498dae99e3b32c4fe83e10cf282e64a04cea
IVAN2001ESP <famigoro8@gmail.com>
2021-05-07 16:59:11 +0200

    Foro,Audioteca

---
+++ src/validar.php
@@ -0,0 +1,31 @@
+<?php
+             
+             $usuario=$_POST['username'];
+             $contraseña=$_POST['password'];
+             $contraseñarep=$_POST['passwordrep'];
+             if($contraseña==$contraseñarep){
+             session_start();
+             $_SESSION['usuario']=$usuario;
+             include('db.php');
+             $consulta="SELECT*FROM user where  Username='$usuario' and Contra='$contraseña'";
+             $resultado=mysqli_query($conexion,$consulta);
+             $filas=mysqli_num_rows($resultado);
+
+
+             if($filas){
+               header("location:main.html");
+             }else{
+               ?>
+               <?php
+
+               include("index.php")
+               ?>
+               <h1>error autentificacion</h1>
+               <?php
+             }
+            }else{
+                
+            }
+             mysqli_free_result($resultado);
+             mysqli_close($conexion);
+             ?>
