6d3af384383ae014b852361c5d3a14aabab1c3cf
Miguel <miguelmorci@gmail.com>
2021-05-09 15:26:45 +0200

    probando secuencer, failed #1

--- src/estudio/estudio.js
+++ src/estudio/estudio.js
@@ -11,18 +11,18 @@ $(document).ready(function () {
 
     $(".fa-play").click(function (ev) {
         ev.preventDefault()
-
+        Tone.start()
     })
 
     $(".fa-stop").click(function (ev) {
         ev.preventDefault()
-        
+        Tone.Transport.pause()
+
     })
 
     /* evento a cada beat para pulsarlo */
     $(".beat").click(function (ev) {
         ev.preventDefault()
-        console.log(this);
         $(this).toggleClass("marked")
         leerMarked()
     })
@@ -32,9 +32,55 @@ $(document).ready(function () {
         for (const pista in track) {
             for (let i = 1; i < 33; i++) {
                 $(`.${pista}>.beat.${i}`).hasClass("marked") ?
-                    track[pista][i-1] = 1 : track[pista][i-1] = 0
+                    track[pista][i - 1] = 1 : track[pista][i - 1] = 0
             }
         }
         console.log(track);
     }
+
+    /* 
+        //AÑADIIIDO UPDATE: there is a problem in chrome with starting audio context
+        //  before a user gesture. This fixes it.
+        document.documentElement.addEventListener('mousedown', () => {
+            if (Tone.context.state !== 'running') Tone.context.resume();
+        });
+
+        const synths = [
+            new Tone.Synth(),
+            new Tone.Synth(),
+            new Tone.Synth(),
+            new Tone.Synth()
+        ];
+
+        synths[0].oscillator.type = 'triangle';
+        synths[1].oscillator.type = 'sine';
+        synths[2].oscillator.type = 'sawtooth';
+        synths[3].oscillator.type = 'sine';
+
+        const gain = new Tone.Gain(0.6);
+        gain.toDestination();
+
+        synths.forEach(synth => synth.connect(gain));
+
+        const $rows = $('.track'),
+            notes = ['G5', 'E4', 'C3', 'F3'];
+        let index = 0;
+        Tone.Transport.scheduleRepeat(repeat, '8n');
+        Tone.Transport.start();
+
+        function repeat(time) {
+            let step = index % 32;
+            for (let i = 0; i < $rows.length; i++) {
+                let synth = synths[i],
+                    note = notes[i],
+                    row = $rows[i];
+
+                let inst = $(row).attr('class').split(' ')[1]
+                console.log(inst);
+                let input = $(`${inst}>.beat.${step+1}`)
+                console.log(input);
+                if ($(`${inst}>.beat.${step+1}`).hasClass("marked")) synth.triggerAttackRelease(note, '8n', time);
+            }
+            index++;
+        } */
 })
--- src/testing.js
+++ src/testing.js
@@ -3,39 +3,41 @@ console.clear();
 //AÑADIIIDO UPDATE: there is a problem in chrome with starting audio context
 //  before a user gesture. This fixes it.
 document.documentElement.addEventListener('mousedown', () => {
-  if (Tone.context.state !== 'running') Tone.context.resume();
+    if (Tone.context.state !== 'running') Tone.context.resume();
 });
 
 const synths = [
-  new Tone.Synth(),
-  new Tone.Synth(),
-  new Tone.Synth()
+    new Tone.Synth(),
+    new Tone.Synth(),
+    new Tone.Synth(),
+    new Tone.Synth()
 ];
 
 synths[0].oscillator.type = 'triangle';
 synths[1].oscillator.type = 'sine';
 synths[2].oscillator.type = 'sawtooth';
+synths[3].oscillator.type = 'sine';
 
 const gain = new Tone.Gain(0.6);
 gain.toMaster();
 
 synths.forEach(synth => synth.connect(gain));
 
-const $rows = document.body.querySelectorAll('div > div'),
-      notes = ['G5', 'E4', 'C3'];
+const $rows = document.body.querySelectorAll('.track'),
+    notes = ['G5', 'E4', 'C3', 'F3'];
 let index = 0;
 
 Tone.Transport.scheduleRepeat(repeat, '8n');
 Tone.Transport.start();
 
 function repeat(time) {
-  let step = index % 8;
-  for (let i = 0; i < $rows.length; i++) {
-    let synth = synths[i],
-        note = notes[i],
-        $row = $rows[i],
-        $input = $row.querySelector(`input:nth-child(${step + 1})`);
-    if ($input.checked) synth.triggerAttackRelease(note, '8n', time);
-  }
-  index++;
+    let step = index % 32;
+    for (let i = 0; i < $rows.length; i++) {
+        let synth = synths[i],
+            note = notes[i],
+            $row = $rows[i],
+            $input = $row.querySelector(`.beat.${step + 1}`);
+        if ($input.className == "marked") synth.triggerAttackRelease(note, '8n', time);
+    }
+    index++;
 }
