512ce32f6ef9fed580e0854b5c50dd8f3192bfdb
Miguel <miguelmorci@gmail.com>
2021-06-04 16:02:00 +0200

    main arreglado, comentado lo que no debia

--- src/main/main.js
+++ src/main/main.js
@@ -1,9 +1,9 @@
 //esto era para poner un gif de carga si tardaban las iamgenes, pero claro. lo q tarda es el propio gif ahora
+// $('#loading').hide();
 
-/* $(document).ready(function () {
-    $('#loading').hide();
+$(document).ready(function () {
     $(".boton").mouseover(function () {
         $(".boton").removeClass("show");
         $(this).addClass("show");
     })
-}) */
+})
