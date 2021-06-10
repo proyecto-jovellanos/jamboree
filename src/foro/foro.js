9bf761b10f7f13325990d588025dd2e902b6f3c5
Miguel <miguelmorci@gmail.com>
2021-06-10 21:18:55 +0200

    barras funcando, mejorar colores y el timeline

--- src/foro/foro.js
+++ src/foro/foro.js
@@ -11,22 +11,19 @@ $(document).ready(function () {
     $("canvas").each(function () {
         let id = $(this).attr("id")
         let track = JSON.parse($(this).prev().html())
-        // console.log(track);
         for (let i = 0; i < 4; i++) {
             for (let j = 0; j <= 31; j++) {
                 if (track[i][0][j] == 1) {
-                    //console.log(track[i][0][j]);
                     draw(i, j, id)
                 }
-                //else
-                //    console.log("JSONN");
-                //console.log(track[i][0][j]);
             }
         }
     });
 
+    //@param i=0,1,2,3 ; j= 0-31 
     function draw(i, j, id) {
-        var colores = ["#6D5FF5", "#DE6262", "#F5CA5F", "#6DEB5B"]
+        //var colores = ["#6D5FF5", "#DE6262", "#F5CA5F", "#6DEB5B"]
+        var colores = ["#412166", "#5504B3", "#6E05E6", "#954BEA"]
         var canvas = document.getElementById(`${id}`)
         var ctx = canvas.getContext("2d");
 
@@ -35,12 +32,15 @@ $(document).ready(function () {
         beatwidth es el ancho del canvas / cada hueco de los beats */
         let beatWidth = canvas.width / 32
         let x = beatWidth * j
+        let beatHeight = canvas.height / 4
+        //let y = beatHeight * i+canvas.height
+        let y = (beatHeight * i)
 
         /* quiero q se dibuje la track que corresponda
          si i es 1 que se dibuja la barra de snare. 
         beatwidth es el ancho del canvas / cada hueco de los beats */
         ctx.fillStyle = colores[i];
-        ctx.fillRect(x, canvas.height / 2, beatWidth, canvas.height / 2);
+        ctx.fillRect(x, y, beatWidth, canvas.height);
     }
 
 
@@ -59,9 +59,9 @@ $(document).ready(function () {
             track = $(this).prev().prev().html()
             bpm = $(this).prev().prev().prev().html()
         }
-       /*  console.log(id);
-        console.log(bpm);
-        console.log(track); */
+        /*  console.log(id);
+         console.log(bpm);
+         console.log(track); */
 
         var params = {
             method: 'POST',
